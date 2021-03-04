b5ce63f5dabf9bf04ea88bea4c79f72d9fd4431e
Bao Dang <danglequocbao001@gmail.com>
2021-03-04 22:38:52 +0700

    init some module, component

--- bessenger/App.js
+++ bessenger/App.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Loader } from './src/component';
 import Nav from './src/navigation'
 
-const App = () => <Nav/>
+const App = () => (
+    <Fragment>
+        <Nav/>
+        <Loader/>
+    </Fragment>
+)
 
 export default App
